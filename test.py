0edaf9cf8481d5d67faf670537c087f7084d11b3
kaiyu <siriusxky@gmail.com>
2021-03-15 18:46:18 +0800

    Minor modify

--- test.py
+++ test.py
@@ -3,18 +3,18 @@ import numpy as np
 from time import time
 from src.prefix_beam_search import decode
 
-sys.path.append('./cmake-build-debug')
+sys.path.append('./cmake-build-release')
 import ctcdecoder
 
 if __name__ == '__main__':
     np.random.seed(3)
 
-    time_len = 30
-    output_dim = 20
+    time_len = 100
+    output_dim = 100
 
     probs = np.random.rand(time_len, output_dim)
     probs = probs / np.sum(probs, axis=1, keepdims=True)
-    print('probs: ', probs)
+    # print('probs: ', probs)
     probs = np.log(probs)
 
     print('----------------- python -----------------')
